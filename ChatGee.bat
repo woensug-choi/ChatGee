cb195f6d15ed58e25a72baf1815afd62ffa96381
Woensug Choi <woensug.choi@gmail.com>
2023-04-23 09:03:35 +0900

    ChatGee batch

--- ChatGee.bat
+++ ChatGee.bat
@@ -5,43 +5,59 @@ echo ========== ChatGee AI Chatbot ==========
 
 @REM ------- Install Python if not installed
 
-where python3.10 > nul 2>&1
-
-if %errorlevel% equ 0 (
-    python -c "import platform; assert platform.python_version() == '3.10.10', 'Unexpected Python version'"
-    echo.
-    echo Python 3.10.10 Installation Confirmed !
+if exist venv_chatgee\Scripts\activate.bat (
+  echo File exists
 ) else (
-    echo.
-    echo Pyton 3.10.10 is not installed!. Please install with python-3.10.10.exe
+  call:find_python 
+  call:check_python_exist
+  if "%python%" == "" (
+      pause
+      EXIT /B 0
+  )
+
+  call:find_pip
+
+  if "%pip_bin%" == "" (
+    %python% -m ensurepip --upgrade > nul 2>&1
+  ) else (
+    echo pip3 Installation Confirmed !
+  )
+
+  echo.
+  echo Initiate Virtual Environment for ChatGee
+  %python% -m pip install --upgrade pip > nul 2>&1
+  %pip_bin% install virtualenv > nul 2>&1
+  %python% -m venv venv_chatgee > nul 2>&1
 )
 
-@REM ----- Create Virtual Env For ChatGee
-
-where pip > nul 2>&1
-
-if %errorlevel% equ 0 (
-    goto :pip_installed
-) else (
-    python -m ensurepip --upgrade > nul 2>&1
-)
-
-:pip_installed
-echo.
-echo Initiate Virtual Environment for ChatGee
-python -m pip install --upgrade pip > nul 2>&1
-pip install virtualenv > nul 2>&1
-python -m venv venv_chatgee > nul 2>&1
 call venv_chatgee\Scripts\activate.bat > nul 2>&1
 
+call :check_ngrok
+
 @REM ----- Install ChatGee
-echo.
-echo ========== ChatGee AI Chatbot ==========
 echo.
 echo ========= Virtual Environment ==========
 echo.
 echo (1/4) Install ChatGee Library
 pip install -r requirements.txt > nul 2>&1
+
+for /f "tokens=* USEBACKQ" %%F in (`.\yq.exe eval ".OPEN_AI.API_KEY" settings.yaml`) do (
+  set "open_api_key=%%F"
+)
+for /f "tokens=* USEBACKQ" %%F in (`.\yq.exe eval ".NGROK.TOKEN" settings.yaml`) do (
+  set "ngrok_token=%%F"
+)
+
+if "%open_api_key%" == "YOUR_OPEN_AI_API_KEY" (
+  echo You should input OPEN_API "API_KEY" in settings.yaml
+  pause
+  EXIT /B 0
+)
+if "%ngrok_token%" == "YOUR_OPEN_AI_API_KEY" (
+  echo You should input NGROK "TOKEN" in settings.yaml
+  pause
+  EXIT /B 0
+)
 
 @REM ---- RUN SERVEO TUNNEL
 echo.
@@ -56,46 +72,51 @@ for /f "tokens=* USEBACKQ" %%F in (`.\yq.exe eval ".SERVER.PORT_NUMBER" settings
 )
 
 echo.
-echo (4/4) Run Localhost.run Tunnel
-
-setlocal enabledelayedexpansion
-
-set "tempFile1=%TEMP%\tunnel_output_temp.txt"
-set "tempFile2=tunnel_output.txt"
-
-taskkill /im ssh.exe /f >nul 2>&1
-start /b /min cmd /c "ssh -o StrictHostKeyChecking=no -R 80:127.0.0.1:%port_number% nokey@localhost.run > %tempFile1% 2>&1"
-
-:wait_output
-timeout /t 5 /nobreak >nul
-if not exist %tempFile1% goto wait_output
-copy %tempFile1% %tempFile2% >nul 2>&1
-
-:parse_output
-set "serveo_addr="
-for /f "tokens=1,2,3" %%a in ('type %tempFile2% ^| findstr "tunneled with tls termination"') do (
-    set "serveo_addr=https://%%a"
-    goto serveo_found
-)
-
-:serveo_found
-if not defined serveo_addr (
-  timeout /t 5 /nobreak >nul
-  goto parse_output
-) else (
-  echo.
-  echo ===========    SUCCESS !!   ============
-  echo.
-  echo The ChatGee Server Address : %serveo_addr%
-  echo The ChatGee Prompt Skill : %serveo_addr%/prompt
-  echo.
-  echo To test at local machine,
-  echo Goto %serveo_addr%/local_test
-  echo Or http://localhost:%port_number%/local_test
-  echo.
-  echo If none of above works, it could be firewall problem
-  echo.
-  echo Change settings.yaml for system prompts and other settings.
-)
+echo "(4/4) Run ngrok"
+.\ngrok.exe config add-authtoken %ngrok_token%
+.\ngrok.exe http %port_number%
 
 pause
+goto :eof
+
+:find_python
+set "python="
+for /f "tokens=* USEBACKQ" %%F in (`where python`) do (
+  for /f "tokens=* USEBACKQ" %%G in (`%%F -c "import sys;print(sys.version_info.minor==10, end='')" 2^>^&1`) do (
+    if "%%G" == "True" (
+      set "python=%%F"
+      goto :break0
+    )
+  )
+)
+:break0
+EXIT /B 0
+:check_python_exist
+if "%python%" == "" (
+  echo.
+  echo Pyton 3.10 is not installed!. Please install with python-3.10.10.exe
+) else (
+  echo.
+  echo Python 3.10.10 Installation Confirmed !
+)
+EXIT /B 0
+:find_pip
+set "pip_bin="
+for %%F in (%python%) do set dirname=%%~dpF
+for /f "tokens=* USEBACKQ" %%F in (`where pip`) do (
+  echo %%F | findstr %dirname% >nul
+  if not errorlevel 1 (
+    set "pip_bin=%%F"
+    goto :break1
+  )
+)
+:break1
+EXIT /B 0
+
+:check_ngrok
+if not exist ngrok.exe (
+  echo ========== Downloading ngrok ==========
+  curl -o ngrok-v3-stable-windows-amd64.zip https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-windows-amd64.zip
+  tar -xf .\ngrok-v3-stable-windows-amd64.zip
+)
+EXIT /B 0
